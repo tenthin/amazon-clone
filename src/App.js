ab8d8c1ead35f86118d8c0d85bbba25518920aca
Tenzin <tenthinten@gmail.com>
2022-02-21 22:01:00 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./Components/Home"
 function App() {
   return (
-    <div className="App">
-      <Switch>
-      <Route path ="/" component={Home}/>
-      </Switch>      
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+        </Switch>      
+      </div>
+    </Router>
+
   );
 }
 
