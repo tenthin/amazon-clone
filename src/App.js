75f17fcaa6ccec7395ab9f0b5264b5f123fb2029
Tenzin <tenthinten@gmail.com>
2022-02-21 22:05:46 +0530

    enable route path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./Components/Home"
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          <Route path="/">
+            <h1>HOME PAGE</h1>
+          </Route>
         </Switch>      
       </div>
     </Router>
