118a6860a27a6ddba3464542860762e6d00cf5a1
Tenzin <tenthinten@gmail.com>
2022-02-21 22:16:34 +0530

    create header for amazon

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className="header">
+        {/* logo on the left -->img */}
+            <img 
+                className="header__logo" 
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                alt="" 
+            />
+
+        {/* Search box */}
+        {/* 3 links */}
+        {/* basket icon with number */}
+    </nav>
+  )
+}
+
+export default Header
