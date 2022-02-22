e8509244c47d30b7c0ff2df09630091d43d66d27
Tenzin <tenthinten@gmail.com>
2022-02-22 16:51:19 +0530

    add link to logo

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Header.css";
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
     <nav className="header">
         {/* logo on the left -->img */}
+        <Link to="/">
             <img 
                 className="header__logo" 
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
                 alt="" 
             />
+        </Link>
 
         {/* Search box */}
         {/* 3 links */}
