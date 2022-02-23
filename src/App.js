0369f2acfcad2e62985469d8b46517b7591cfe02
Tenzin <tenthinten@gmail.com>
2022-02-23 14:31:45 +0530

    add cover page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
+
+
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
+          <Header/>
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -14,43 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header/>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
+            <Home/>
           </Route>
         </Switch>      
       </div>
--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,6 @@ import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
   return (
     <nav className="header">
-        {/* logo on the left -->img */}
         <Link to="/">
             <img 
                 className="header__logo" 
@@ -15,15 +14,12 @@ function Header() {
             />
         </Link>
 
-        {/* Search box */}
         <div className="header__search">
             <input type="text" className="header__searchInput" />
             <SearchIcon className="header__searchIcon"/>
         </div>
 
-        {/* 3 links */}
         <div className="header__nav">
-            {/* 1st link */}
             <Link to="/login" className="header__link">
                 <div className="header__option">
                     <span className="header__optionLineOne">Hello Tenzin</span>
@@ -31,7 +27,6 @@ function Header() {
                 </div>
             </Link>
 
-            {/* 2nd link */}
             <Link to="/" className="header__link">
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
@@ -39,7 +34,6 @@ function Header() {
                 </div>
             </Link>
 
-            {/* 3rd link */}
             <Link to="/" className="header__link">
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
@@ -47,18 +41,14 @@ function Header() {
                 </div>
             </Link>
 
-            {/* 4th link */}
                 <Link to="/checkout" className="header__link">
                     <div className="header__optionBasket">
-                        {/* Shopping basket icon */}
                         <ShoppingBasketIcon/>
-                        {/* no of items in basket */}
                         <span className="header__optionLineTwo header__basketCount">0</span>
                     </div>
                 </Link>
 
         </div>
-        {/* basket icon with number */}
     </nav>
   )
 }
