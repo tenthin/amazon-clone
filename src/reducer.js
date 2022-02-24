43cabdbbd4a32c0ae42bc52e0c5ce5c6c3366f2a
Tenzin <tenthinten@gmail.com>
2022-02-24 19:24:38 +0530

    number in basket increase

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,10 @@ const reducer = (state, action) => {
     switch(action.type) {
         case 'ADD_TO_BASKET':
             // Logic for adding item to basket
-            return {state}
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            }
         case 'REMOVE_TO_BASKET':
             // Logic for Removing item from basket
             return {state}
