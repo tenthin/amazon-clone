6ebcfe4c3825bdda956823e80ec8ef4e92cdbf84
Tenzin <tenthinten@gmail.com>
2022-02-24 15:11:02 +0530

    add reducer to add and remove item from basket

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { StateProvider } from './StateProvider';
-import {initialState} from './reducer';
+import reducer, {initialState} from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={}>
+    <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>  
   </React.StrictMode>,
--- src/reducer.js
+++ src/reducer.js
@@ -5,8 +5,16 @@ export const initialState = {
 function reducer(state, action) {
     switch(action.type) {
         case 'ADD_TO_BASKET':
-        // Logic for adding item to basket
+            // Logic for adding item to basket
+            break;
+        case 'REMOVE_TO_BASKET':
+            // Logic for Removing item from basket
+            break;
+        default:
+            return state;
     }
 }
+
+export default reducer;
 /*
 */
