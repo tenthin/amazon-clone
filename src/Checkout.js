690b5a3b531694287169913f9bf9373cf97d9a16
Tenzin <tenthinten@gmail.com>
2022-02-25 14:54:11 +0530

    add item in basket and display if its empty or has item in it

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useStateValue} from './StateProvider';
+import './Checkout.css';
 
 function Checkout() {
     const [{basket}] = useStateValue();
@@ -11,6 +12,15 @@ function Checkout() {
             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
             alt="" 
         />
+        {basket?.length === 0 ? (
+            <div>
+                <h2>Your Shopping Basket is empty</h2>
+            </div>
+        ) : (
+            <div>
+                <h2>Your Shopping Basket</h2>
+            </div>
+        )} 
     </div>
   )
 }
