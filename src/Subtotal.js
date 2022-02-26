93927af6d7b5bd95eea2e0762435849f1391ac86
Tenzin <tenthinten@gmail.com>
2022-02-26 16:00:19 +0530

    add checkout box

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import "./Subtotal.css";
 import CurrencyFormat from 'react-currency-format';
-
+import {useStateValue} from "./StateProvider";
 
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
   return (
     <div className="subtotal">
         {/* price */}
 
         <CurrencyFormat
-
             renderText={(value) = (
                 <>
                     <p>
-                        Subtotal ({basket.length} items) : <strong>{value}</strong>
+                        Subtotal ({basket.length} items): <strong>{value}</strong>
                     </p>
                     <small className="subtotal__gift">
                         <input type="checkbox" /> This order contains a gift
                     </small>
                 </>
             )}
-
             decimalState={2}
-            value={}
+            value={0}
             displayType={'text'}
             thousandsSeparator={true}
             prefix={"$"}
@@ -32,4 +31,4 @@ function Subtotal() {
   )
 }
 
-export default Subtotal
+export default Subtotal;
